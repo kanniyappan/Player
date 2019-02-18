84ad6977a20c073c0f0bb5fc145d0739e79a5ea3
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-02-18 06:13:16 -0800

    Create P30.C

---
+++ P30.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+main() 
+{
+	char s[50];
+	int i,n;
+	scanf("%s",s);
+	n=strlen(s);
+	for(i=0;i<n;i++)
+	{
+		s[i]=s[i]+3;
+		if(s[i]=='X')
+		s[i]='A';
+		if(s[i]=='Y')
+		s[i]='B';
+		if(s[i]=='Z')
+		s[i]='C';
+	}
+	printf("%s",s);
+}
