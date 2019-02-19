cded07764aaa837d3f1d1660b1bdce60472b7a62
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-02-19 15:10:43 +0530

    Create P29.C

---
+++ P29.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main() 
+{
+	int n,i,j,flag=1;
+	scanf("%d",&n);
+	for(j=2;j<=n;j++)
+	{
+		if(n%j==0)
+		{
+			flag=1;
+			for(i=2;i<=j/2;i++)
+			{
+			if(j%i==0)
+			{
+			flag=0;
+			break;
+			}
+			}
+	if(flag==1)
+	{
+		printf("%d ",j);
+	}
+}
+	}
+}
