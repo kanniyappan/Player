16a4d0763ceaea23c679619d5ed5d454a84da0fa
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-02-19 06:06:28 -0800

    Create P37.C

---
+++ P37.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+    char a[30];
+    int i;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>96)
+		{
+			a[i]=a[i]-32;
+		}
+		else if(a[i]<91)
+		{
+			a[i]=a[i]+32;
+		}
+	}
+    printf("%s",a);
+}
