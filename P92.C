93a1d7c18089adb99f895e47464162b2b4274e67
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:08:31 +0530

    Create P92.C

---
+++ P92.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n,i,h;
+    int a[10];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+			scanf("%d",&a[i]);
+	h=a[0];
+	for(i=0;i<n;i++)
+		h=h&a[i];
+	printf("%d",h);
+}
