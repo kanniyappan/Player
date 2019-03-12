3f6fda85c1a64b8174c9b4d43d8079915d9314b9
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:23:05 +0530

    Create P110.C

---
+++ P110.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+
+
+
+
+main()
+{
+    long int n, hex=0,rem,p=1;
+    scanf("%ld",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        hex+=rem*p;
+        p=p*2;
+        n=n/10;
+    }
+    printf("%lx",hex);
+    
+}
