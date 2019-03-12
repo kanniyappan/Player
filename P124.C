02f42c6b2d5fd92eb760398c5c6ef1d088b51e35
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:33:58 +0530

    Create P124.C

---
+++ P124.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+
+main()
+{
+    long int a,b,c,ans,s=1,i;
+    scanf("%ld %ld %ld",&a,&b,&c);
+    for(i=1;i<=b;i++)
+    s*=a;
+    ans=s%c;
+    printf("%ld",ans);
+    
+    
+    
+    }
