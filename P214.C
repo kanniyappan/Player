14c6e68d0528cd42eb8dab7f15d82122fddb4e60
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:38:23 +0530

    Create P214.C

---
+++ P214.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+
+
+main()
+{
+    int a[30],i,n,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        {
+            sum+=a[i];
+        }
+    }
+    
+    
+    printf("%d",sum);
+}
+
