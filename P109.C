a4f775e59434220a41b9978b3b74c3f2da3704cc
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:22:23 +0530

    Create P109.C

---
+++ P109.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+
+main()
+{
+    long int n, oct=0,rem,p=1;
+    scanf("%ld",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        oct=oct+rem*p;
+        p=p*2;
+        n=n/10;
+    }
+    printf("%lo",oct);
+    
+}
