57a17893e20435adc89ae7872854923ff071966d
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:21:15 +0530

    Create P107.C

---
+++ P107.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+
+main()
+{
+    long int a,b,sum=0,i;
+    scanf("%ld %ld",&a,&b);
+    for(i=a;i<=b;i++)
+        if(i%2!=0)
+            sum=sum+i;
+    printf("%ld",sum);
+}
