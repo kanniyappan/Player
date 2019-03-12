f8ae8ba76bd480dab537e419a54ffeae6cbeb982
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-12 15:18:10 +0530

    Create P106.C

---
+++ P106.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+main()
+{
+    long int n,t,c=1;
+    int sum,rem,a,b;
+    scanf("%ld",&n);
+    t=n;
+    while(t>0)
+    {
+        t/=10;
+        c*=10;
+    }
+    c=c/10;
+        a=n/c;
+        b=n%10;
+        sum=a+b;
+    printf("%d ",sum);
+}
