494adfc9d72eef84c405ff113842260228262b12
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-18 08:01:17 -0700

    Create P208.C

---
+++ P208.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+    int n,a[30],i,max=0,min=0,x=0,y=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    max=a[0];
+    min=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+        max=a[i];
+        x=i;
+        }
+        else if(a[i]<min)
+        {
+        min=a[i];
+        y=i;
+        }
+    }
+    printf("%d",x-y);
+ }
+  
