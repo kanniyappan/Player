811271c2142957c41875bea812892e82de9dbd31
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-03-18 08:10:17 -0700

    Create P210.C

---
+++ P210.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+char a[20];
+int i,j,c=0;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+c++;
+for(i=0;i<c;i++)
+{
+for(j=i+1;j<c;j++)
+{
+if(a[i]==a[j])
+{
+a[j]='\0';
+}
+}
+}
+j=0;
+for(i=0;i<c;i++)
+{
+if(a[i]!='\0')
+{
+a[j]=a[i];
+j++;
+}
+}
+a[j]='\0';
+printf("%s",a);
+}
